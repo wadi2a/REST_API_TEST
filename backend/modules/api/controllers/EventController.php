b7cce4938c8aaa4003c61132aaa47f76dd8e80d8
wadi2a <ben.aissa.ouadie@gmail.com>
2018-04-12 05:27:52 +0200

    creation d'un api rest avec yii framework

---
+++ backend/modules/api/controllers/EventController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace backend\modules\api\controllers;
+use backend\modules\api\models\Event;
+
+class EventController extends \yii\web\Controller
+{   public $enableCsrfValidation = false;
+    public function actionIndex()
+    {   
+        //si c'est un post 
+        if(\Yii::$app->request->post())
+        {
+            \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+         $event = new Event();
+     $event->scenario = Event::SCENARIO_CREATE;
+     $event->attributes= \Yii::$app->request->post();
+     
+     if($event->validate()){
+         $event->save();
+         return array('status' => TRUE,'data' => 'event created successfully');
+     }else{
+         return array('status' => FALSE, 'data' => $event->getErrors());
+     }
+     
+            
+        }else{
+            
+            //si on cherche les events
+                 \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+     $event = Event::find()->all();
+     
+     if(count($event)>0){
+         return array('status' =>TRUE,'data'=>$event);
+         
+     }else{
+         return array ('status' =>FALSE,'data' => 'No events found');
+     }
+        
+        }
+   
+        
+     
+    
+     
+    
+     
+        return $this->render('index');
+    }
+public function actionCreateEvent(){
+    \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+     $event = new Event();
+     $event->scenario = Event::SCENARIO_CREATE;
+     $event->attributes= \Yii::$app->request->post();
+     
+     if($event->validate()){
+         $event->save();
+         return array('status' => TRUE,'data' => 'event created successfully');
+     }else{
+         return array('status' => FALSE, 'data' => $event->getErrors());
+     }
+}
+
+public function  actionListEvent(){
+    \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+     $event = Event::find()->all();
+     
+     if(count($event)>0){
+         return array('status' =>TRUE,'data'=>$event);
+         
+     }else{
+         return array ('status' =>FALSE,'data' => 'No events found');
+     }
+}
+}
---
+++ backend/modules/api/models/Event.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace backend\modules\api\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "event".
+ *
+ * @property string $name
+ * @property string $cookie_Id
+ * @property string $referrer
+ * @property string $createdAt
+ */
+class Event extends \yii\db\ActiveRecord
+{   const SCENARIO_CREATE = 'create';
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'event';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'cookie_Id', 'referrer', 'createdAt'], 'required'],
+            [['createdAt'], 'safe'],
+            [['name', 'cookie_Id', 'referrer'], 'string', 'max' => 100],
+        ];
+    }
+    public function scenarios(){
+        $scenarios = parent::scenarios();
+        $scenarios['create']=['name','cookie_id','referrer','createdAt'];
+        return $scenarios;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'Name',
+            'cookie_Id' => 'Cookie  ID',
+            'referrer' => 'Referrer',
+            'createdAt' => 'Created At',
+        ];
+    }
+}
